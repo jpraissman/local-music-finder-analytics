0f17c404e89b01e1654bb91faf7d13c2a015c253
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-12-13 13:37:15 -0500

    @Valid and @RequestBody for all routes

--- src/main/java/com/thelocalmusicfinder/localmusicfinderanalytics/controllers/ClientController.java
+++ src/main/java/com/thelocalmusicfinder/localmusicfinderanalytics/controllers/ClientController.java
@@ -3,14 +3,12 @@ package com.thelocalmusicfinder.localmusicfinderanalytics.controllers;
 import com.thelocalmusicfinder.localmusicfinderanalytics.dto.GetCampaignDTO;
 import com.thelocalmusicfinder.localmusicfinderanalytics.dto.GetLinkResponseDTO;
 import com.thelocalmusicfinder.localmusicfinderanalytics.services.CampaignService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -21,7 +19,7 @@ public class ClientController {
     private final CampaignService campaignService;
 
     @PostMapping("/campaign/get")
-    public ResponseEntity<GetLinkResponseDTO> getCampaign(GetCampaignDTO payload) {
+    public ResponseEntity<GetLinkResponseDTO> getCampaign(@Valid @RequestBody GetCampaignDTO payload) {
         try{
             GetLinkResponseDTO res = campaignService.getLink(payload);
             return new ResponseEntity<>(res, HttpStatus.OK);
